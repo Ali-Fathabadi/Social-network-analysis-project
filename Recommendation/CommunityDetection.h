37fcd19a2889aa830364811285c2e570f60b3970
SedMahdiD <sedmahdid@gmail.com>
2026-08-03 18:58:40 +0330

    Update CommunityDetection.h

--- Recommendation/CommunityDetection.h
+++ Recommendation/CommunityDetection.h
@@ -1,1 +1,17 @@
+#pragma once
+#include "Graph.h"
+#include <string>
+#include <vector>
 
+namespace algo {
+
+struct Community
+{
+    int id;
+    std::vector<std::string> members;
+};
+
+std::vector<Community>
+communityDetection(const Graph& g);
+
+}
