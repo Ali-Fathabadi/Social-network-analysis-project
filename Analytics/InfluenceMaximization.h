c9ee573d7a6b60e95bb8c77fd13bcb3d8cb6ed2c
nargesnazemi403 <nargesnazemi16@gmail.com>
2026-08-03 21:55:58 +0330

    Update InfluenceMaximization.h

--- Analytics/InfluenceMaximization.h
+++ Analytics/InfluenceMaximization.h
@@ -1,1 +1,13 @@
+#pragma once
+#include "Graph.h"
+#include <string>
+#include <vector>
 
+namespace algo {
+
+// انتخاب K کاربر بهینه برای شروع انتشار خبر:
+//  اولویت اول: بیشترین تعداد افراد شبکه در نهایت خبر را دریافت کنند
+//  اولویت دوم: خبر در کمترین زمان ممکن به آن افراد برسد
+std::vector<std::string> optimizeNewsSpread(const Graph& g, int k);
+
+}
