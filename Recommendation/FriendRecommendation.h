43ecadd5225aa1b84a130f613f3f37e6b2b4ce0f
SedMahdiD <sedmahdid@gmail.com>
2026-08-03 18:54:58 +0330

    Update FriendRecommendation.h

--- Recommendation/FriendRecommendation.h
+++ Recommendation/FriendRecommendation.h
@@ -1,1 +1,16 @@
+#pragma once
+#include "Graph.h"
+#include <string>
+#include <vector>
 
+namespace algo {
+
+struct FriendSuggestion {
+    std::string id;
+    int mutualCount;
+};
+
+std::vector<FriendSuggestion>
+recommendFriends(const Graph& g, const std::string& userId);
+
+}
