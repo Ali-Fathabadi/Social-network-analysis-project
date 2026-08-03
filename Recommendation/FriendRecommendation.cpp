b12b38b071b91d88922e741151a9941b80038fb6
SedMahdiD <sedmahdid@gmail.com>
2026-08-03 18:56:51 +0330

    Update FriendRecommendation.cpp

--- Recommendation/FriendRecommendation.cpp
+++ Recommendation/FriendRecommendation.cpp
@@ -1,1 +1,36 @@
+#include "FriendRecommendation.h"
+#include <unordered_map>
+#include <algorithm>
 
+namespace algo {
+
+std::vector<FriendSuggestion>
+recommendFriends(const Graph& g, const std::string& userId)
+{
+    std::vector<FriendSuggestion> result;
+
+    if (!g.findUser(userId))
+        return result;
+
+    const auto& myFriends = g.getFriends(userId);
+
+    std::unordered_map<std::string,int> score;
+
+    // دوستانِ دوستان
+    for (const std::string& f : myFriends)
+    {
+        for (const std::string& candidate : g.getFriends(f))
+        {
+            if (candidate == userId)
+                continue;
+
+            if (myFriends.count(candidate))
+                continue;
+
+            score[candidate]++;
+        }
+    }
+
+    
+
+}
