31042502be349559b84da9a06547b9be3720c211
Arjp80 <ajjj47726@gmail.com>
2026-08-05 15:44:59 +0330

    Update Graph.h

--- Core/Graph.h
+++ Core/Graph.h
@@ -1,12 +1,14 @@
 #pragma once
+
 #include "User.h"
+
+#include <cstddef>
+#include <string>
 #include <unordered_map>
 #include <unordered_set>
-#include <string>
 
 class Graph {
 public:
-
     bool addUser(const std::string& id, const std::string& name);
     bool removeUser(const std::string& id);
     bool editUser(const std::string& id, const std::string& newName);
@@ -14,18 +16,20 @@ public:
     bool addFriendship(const std::string& id1, const std::string& id2);
     bool removeFriendship(const std::string& id1, const std::string& id2);
 
-
     bool findUser(const std::string& id) const;
     const User* getUser(const std::string& id) const;
     bool areFriends(const std::string& id1, const std::string& id2) const;
+
     const std::unordered_map<std::string, User>& getAllUsers() const;
     const std::unordered_set<std::string>& getFriends(const std::string& id) const;
     const std::unordered_map<std::string, std::unordered_set<std::string>>& getAdjacency() const;
-    size_t userCount() const { return users.size(); }
 
-    // ذخیره و بازیابی JSON
-    bool saveToFile(const std::string& path) const;
-    bool loadFromFile(const std::string& path);
+    size_t userCount() const { return users.size(); }
+    size_t friendshipCount() const;
+
+    bool loadFromFile(const std::string& path, std::string* error = nullptr);
+
+    bool saveToFile(const std::string& path, std::string* error = nullptr) const;
 
 private:
     std::unordered_map<std::string, User> users;
